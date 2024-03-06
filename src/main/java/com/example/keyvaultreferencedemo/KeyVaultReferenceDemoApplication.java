88e637584ddd8a89ed74c128b2d87058a15b659e
skmonjurul <monjurulhaque2011@gmail.com>
2024-03-06 21:51:37 +0530

    initial project set up

---
+++ src/main/java/com/example/keyvaultreferencedemo/KeyVaultReferenceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyvaultreferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyVaultReferenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyVaultReferenceDemoApplication.class, args);
+	}
+
+}
